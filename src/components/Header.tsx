376db2de186520d09f657cc78709c72214037a95
felixxleandro <62638333+felixxleandro@users.noreply.github.com>
2025-06-18 14:29:16 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,20 @@ interface HeaderProps {
   titleColor?: "default" | "mixed";
 }
 
-export const Header = ({ title, subtitle, titleColor = "default" }: HeaderProps): JSX.Element => {
+export const Header = ({
+  title,
+  subtitle,
+  titleColor = "default",
+}: HeaderProps): JSX.Element => {
   return (
-    <header className="relative w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[804px]">
-      {/* Background que escapa do container e ocupa toda a largura da tela */}
-      <div className="fixed top-0 left-0 w-screen h-[500px] sm:h-[600px] md:h-[700px] lg:h-[804px] bg-[url(/foto-painel.png)] bg-cover bg-center -z-10">
-        <div className="h-full bg-[#322030f2]" />
+    <header className="relative w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[804px] overflow-hidden">
+      {/* Background que ocupa toda a largura da tela */}
+      <div className="fixed inset-0 h-[500px] sm:h-[600px] md:h-[700px] lg:h-[804px] bg-[url('/foto-painel.png')] bg-cover bg-center -z-10">
+        <div className="w-full h-full bg-[#322030f2]" />
       </div>
-      
+
       <Navbar />
-      
+
       {/* Hero Content */}
       <div className="absolute w-full px-4 top-[200px] sm:top-[250px] md:top-[300px] lg:top-[358px] left-1/2 transform -translate-x-1/2 text-center z-10">
         <h1 className="font-['Nunito',Helvetica] font-black text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-[86px] tracking-[0] leading-normal">
